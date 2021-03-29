97b26aa6b7cf03bbb1ff9e885aaa28eaa0b1f8ab
Phillip Lakis <p.lakis@icloud.com>
2021-03-29 11:47:30 +1100

    Finance app

---
+++ Financial App/Financial App/Financial_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_AppApp.swift
+//  Financial App
+//
+//  Created by Phillip Lakis on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Financial_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinancialApp/Shared/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  Shared
+//
+//  Created by Phillip Lakis on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
