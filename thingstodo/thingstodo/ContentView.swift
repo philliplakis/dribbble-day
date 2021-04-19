0e9649a3a885e66a41ba238c225da4402142e636
Phillip Lakis <p.lakis@icloud.com>
2021-04-19 12:50:11 +1000

    thingstodo project

---
+++ thingstodo/thingstodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  thingstodo
+//
+//  Created by Phillip Lakis on 19/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ thingstodo/thingstodo/thingstodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  thingstodoApp.swift
+//  thingstodo
+//
+//  Created by Phillip Lakis on 19/4/21.
+//
+
+import SwiftUI
+
+@main
+struct thingstodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
