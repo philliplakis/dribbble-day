03da3ca84f8c8e217fb7fe9ac2e0f62407b222e0
Phillip Lakis <p.lakis@icloud.com>
2021-05-24 14:31:21 +1000

    Orisale

---
+++ orisale/orisale/orisaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  orisaleApp.swift
+//  orisale
+//
+//  Created by Phillip Lakis on 24/5/21.
+//
+
+import SwiftUI
+
+@main
+struct orisaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
